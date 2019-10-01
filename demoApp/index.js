d9f1d33a833a27367589b4d01f5f19b81dd3df16
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2019-10-01 11:17:13 -0500

    setup working export

--- demoApp/index.js
+++ demoApp/index.js
@@ -6,4 +6,11 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import RNFormHelpers from './react-native-form-helpers';
+import {validationDictionary} from './react-native-form-helpers/dictionary';
+
+export const validationService = RNFormHelpers({
+  dictionary: validationDictionary,
+});
+
 AppRegistry.registerComponent(appName, () => App);
